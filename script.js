fe3dcf56c5c6e1b4e8ee85da1c767d1d83288cb1
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-18 19:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getelementById('userform').addEventListener('submit',function(e){
+	e.preventDefault();//prevent default form submission behaviour
+	//get the input values
+	const name=document.getElementById('name').value;
+	const age=document.getElementById('age').value;
+	//validate both fields are empty
+	if(name=""|| age===""){
+		alert('Please fill in both fields.');
+		return;
+	}
+	//create a new promise for the form submission
+	new promise(function(resolve,reject){
+		setTimeout(()=>{
+			if(age>=18{
+				resolve();
+			}
+			else{
+				reject();
+			}
+		},4000);//resolve or reject after 4000 seconds
+			})
+	.then(function(){
+		alert(`Welcome, ${name}. You can vote.`)
+	});
+	.catch(function(){
+		alert(`Oh sorry ${name}. You aren't old enough.`)
+	});
+}
